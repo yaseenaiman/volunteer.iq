0b2edf044aff4d92785a70fef5383fa232376b11
yaseenaiman <yaseenaiman17@gmail.com>
2023-04-20 13:56:15 +0300

    I have added map

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -5,6 +5,8 @@ const tasks = [
       Rating: 7.6,
       Articles: "You are going to be part of a large number of wonderful people, even small efforts are appreciated!",
       Followers: 22,
+      Imgi:"river", 
+
     },
   
     {
@@ -13,13 +15,16 @@ const tasks = [
       Rating: 8.5,
       Articles: "Contribute to our great mission to plant over 2 million trees by 2o25",
       Followers: 22,
+      Imgi:"tree", 
     },
     {
       Id: 3,
       Name: "Nina",
       Rating: 5.6,
-      Articles: 22,
+      Articles: "looool",
       Followers: 22,
+      Imgi:"house", 
+
     },
     {
       Id: 4,
@@ -27,6 +32,8 @@ const tasks = [
       Rating: 10,
       Articles: 44,
       Followers: 22,
+      Imgi:"house", 
+
     },
     {
       Id: 5,
@@ -34,6 +41,8 @@ const tasks = [
       Rating: 9.5,
       Articles: 35,
       Followers: 22,
+      Imgi:"house", 
+
     },
     {
       Id: 5,
@@ -41,6 +50,8 @@ const tasks = [
       Rating: 9.5,
       Articles: 35,
       Followers: 22,
+      Imgi:"house", 
+
     },
   ];
   
--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -166,6 +166,7 @@ const HomePage = () => {
     {/* Copyright */}
   </footer>
   {/*Footer*/}
+  
 </>
 
   );
